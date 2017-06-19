20a2855b0d06a49bc7a601c4683bce8997ff5b92
Sugar1413 <994174408@qq.com>
2017-06-19 10:53:56 +0800

    Initial Commit

---
+++ AFNet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNet
+//
+//  Created by Suger on 17/6/19.
+//  Copyright © 2017年 Suger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AFNet/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AFNet
+//
+//  Created by Suger on 17/6/19.
+//  Copyright © 2017年 Suger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
